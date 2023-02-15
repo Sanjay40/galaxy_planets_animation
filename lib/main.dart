c0cde416514740dcce0d15f768358814976c54b1
Sanjay40 <sanjaytarsariya2004@gmail.com>
2023-02-15 20:11:41 +0530

    Sj || 15/02/2023 08:12 Pm

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:galaxy_planets_animation/screen/home_page.dart';
-import 'package:galaxy_planets_animation/screen/planet_details.dart';
+
 
 
 void main()
 {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
         '/' : (Context) => Galaxy(),
         //'detail' : (context) => const Details(),
--- lib/screen/planet_details.dart
+++ lib/screen/planet_details.dart
@@ -1,5 +1,5 @@
 
-
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:model_viewer_plus/model_viewer_plus.dart';
@@ -7,8 +7,8 @@ import 'package:model_viewer_plus/model_viewer_plus.dart';
 import '../utils/planet_list.dart';
 
 class Details extends StatefulWidget {
-  int? ind;
-  Details({super.key, required int ind});
+  int ind;
+  Details({super.key, required this.ind});
 
   @override
   State<Details> createState() => _DetailsState();
@@ -28,6 +28,7 @@ class _DetailsState extends State<Details> {
         decoration: BoxDecoration(
           image: DecorationImage(
               image: AssetImage('images/back image.jfif'),
+            fit: BoxFit.cover
           )
         ),
         child: SafeArea(
@@ -37,7 +38,7 @@ class _DetailsState extends State<Details> {
             children: [
               const SizedBox(height: 10,),
               Center(
-                child: Text("${Variable.planets[widget.ind!]['name']}",
+                child: Text("${Variable.planets[widget.ind]['name']}",
                   style: GoogleFonts.poppins(
                       fontSize: 22,
                       fontWeight: FontWeight.w600,
@@ -46,12 +47,50 @@ class _DetailsState extends State<Details> {
                   ),
                 ),
               ),
-              const SizedBox(height: 20,),
+              const SizedBox(height: 60,),
               SizedBox(
                 height: 400,
                 child: ModelViewer(
-                  src: Variable.planets[widget.ind!]['file'],
+                  src: Variable.planets[widget.ind]['file'],
                   autoRotate: true,
+                ),
+              ),
+              const SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Container(
+                  height: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xFF052843).withOpacity(0.8),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Description",
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 15,
+                            letterSpacing: 0.5,
+                            color: Colors.grey.shade400,
+                          ),
+                        ),
+                        const SizedBox(height: 10,),
+                        AutoSizeText("${Variable.planets[widget.ind]['des']}",
+                          maxLines: 5,
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16,
+                            letterSpacing: 0.5,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  //height: 100,
                 ),
               ),
             ],
